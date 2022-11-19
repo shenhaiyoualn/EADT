7527d7fff2da375f36e37aba0823daca23dc04ac
shenhaiyoualn <52127848+shenhaiyoualn@users.noreply.github.com>
2022-11-19 16:16:05 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,97 @@
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name pretrain
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name GRID --continue_train --train_epoch 8000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name nose --continue_train --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name eyes  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name nomask  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name nomask  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name mouse  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name all  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name linshi  --train_epoch 100
+
+
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name modify  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name modify_hair_clothes  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name modify_eyes  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name modify_ears  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name modify_nose  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name modify_mouse  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name modify_all  --train_epoch 2000
+
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name modify2_all  --train_epoch 2000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name modify2_hair  --train_epoch 1000
+
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name modify3_all  --train_epoch 1000
+
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all  --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_3 --train_epoch 1000
+
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_3_discrim --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_3_discrim_hair --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_3_discrim_nose --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_3_discrim_mouse --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_3_discrim_eyes --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_3_discrim_eyebrow --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_3_discrim_face --train_epoch 1000
+
+
+
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_5_discrim3 --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_5_discrim4 --train_epoch 1000
+
+#final_rgb加入E1-1
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_5_SPADEUP4+final --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_5_SPADEUP4+final_hair --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_5_SPADEUP4+final_nose --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_5_SPADEUP4+final_eyes --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_5_SPADEUP4+final_mouse --train_epoch 1000
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_modify_all_5_SPADEUP4+final_eyebrow --train_epoch 1000
+
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_test.txt --use_en_feature --use_gmsd  --name final_5_SPADEUP5+finalx2 --train_epoch 1000
+
+#
+# python train.py --dataset_train_list list_train.txt --dataset_test_list list_wild.txt --use_en_feature --use_gmsd  --name linshi
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.logging import TensorBoardLogger
+from config.option import TrainOptions
+from model.total_model.smodel import *
+opt = TrainOptions().parse()
+
+
+def train():
+    model = parsing(opt)
+    save_path = "{}/{}/{}/{}/{}".format(opt.checkpoints_dir, opt.name, opt.ver, opt.dataset_name, opt.log_name)
+    checkpoint = ModelCheckpoint(
+        filepath=save_path,
+        save_top_k=3,
+        save_weights_only=True,
+        monitor='fid',
+        mode='min',
+        verbose=True
+    )
+    logger_path = "{}/{}/{}".format(opt.log_dir, opt.name, opt.dataset_name)
+    logger = TensorBoardLogger(
+        save_dir=logger_path,
+        name=opt.log_name,
+        version=opt.ver
+    )
+    if opt.use_amp:
+        amp_level = 'O2'
+    else:
+        amp_level = 'O0'
+    trainer = Trainer(
+        fast_dev_run=opt.debug,
+        logger=logger,
+        max_epochs=opt.train_epoch,
+        min_epochs=opt.train_epoch,
+        checkpoint_callback=checkpoint,
+        gpus=opt.gpu,
+        check_val_every_n_epoch=opt.every_val_epoch,
+        log_save_interval=2000,
+        use_amp=opt.use_amp,
+        amp_level=amp_level
+    )
+    trainer.fit(model)
+
+
+train()
